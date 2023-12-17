43c1e4f825fe9975822f75bbacd66be43377bcfd
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-12-17 15:55:25 +0300

    layout

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
-
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">
-        protected page
-        <UserButton afterSignOutUrl="/" />
-      </p>
+      <p className="text-3xl font-medium text-sky-700">protected page</p>
     </div>
   )
 }
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from './_components/navbar'
+import Sidebar from './_components/sidebar'
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed flex-col inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
+      <div className="hidden md:flex h-full fixed flex-col  w-56 inset-y-0 z-20">
+        <Sidebar />
+      </div>
+      <main className="md:ml-56 h-full ">{children}</main>
+    </div>
+  )
+}
+
+export default DashboardLayout
