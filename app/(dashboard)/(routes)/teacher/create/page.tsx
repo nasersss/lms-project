9d09ff5bd6f02900268e873184747c9e70894515
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2024-01-21 15:18:11 +0300

    build course creation API

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -38,8 +38,9 @@ const NewCourse = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post('/api/course', values)
+      const response = await axios.post('/api/courses', values)
       router.push(`/teacher/courses/${response.data.id}`)
+      toast.success('Course created successfully')
     } catch (error) {
       toast.error('Oops something is wrong')
     }
